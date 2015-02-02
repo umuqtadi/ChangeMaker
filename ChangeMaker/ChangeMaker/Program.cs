d8f9880e3570e2bb6fc86fbd549f4a848f4fd5d1
Dustin Kraft <kraftdr@gmail.com>
2015-02-02 12:30:46 -0700

    Change Maker Tests

---
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling the function with $4.19.  
+            //Notice that when using the decimal format you must end numbers with an 'm'
+            ChangeAmount(4.19m);
+        }
+
+        public static Change ChangeAmount(decimal amount) 
+        {
+            //this is our object that will hold the data of how many coins of each type to return
+            Change amountAsChange = new Change();
+           
+
+
+            //return our Change Object
+            return changeProvided;
+        }
+
+        /// <summary>
+        /// Example using the Change class to store data
+        /// </summary>
+        public static Change Example(decimal amount)
+        {
+            //creating a new object of our class Change
+            Change amountAsChange = new Change();
+
+            //increasing the number of quarters
+            amountAsChange.Quarters++;
+            amountAsChange.Quarters += 1;
+            amountAsChange.Quarters = amountAsChange.Quarters + 1;
+
+            //outputting to the console
+            Console.WriteLine("Quarters: " + amountAsChange.Quarters);
+
+            //returning the object
+            return amountAsChange;
+        }
+
+    }
+
+    public class Change
+    {
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Quarters { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Dimes to be returned as change
+        /// </summary>
+        public int Dimes { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Nickles to be returned as change
+        /// </summary>
+        public int Nickles { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Pennies to be returned as change
+        /// </summary>public int Nickles { get; set; }
+        public int Pennies { get; set; }
+
+        /// <summary>
+        /// This is a constructor, it initializes a new instance of the class (called an object).  This sets it's default values.
+        /// </summary>
+        public Change()
+        {
+            this.Quarters = 0;
+            this.Dimes = 0;
+            this.Nickles = 0;
+            this.Pennies = 0;
+        }
+    }
+}
---
+++ ChangeMaker/ChangeMaker/Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace ChangeMaker
+{
+    [TestFixture]
+    class Test
+    {
+        [Test,Timeout(500)]
+        public void Test419()
+        {
+            Change amountAsChange = Program.ChangeAmount(4.19m);
+            Assert.IsTrue(amountAsChange.Quarters == 16, "Incorrect Quarters");
+            Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
+            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Pennies == 4, "Incorrect Pennies");
+        }
+        
+        [Test, Timeout(500)]
+        public void Test318()
+        {
+            Change amountAsChange = Program.ChangeAmount(3.18m);
+            Assert.IsTrue(amountAsChange.Quarters == 12, "Incorrect Quarters");
+            Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
+            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Pennies == 3, "Incorrect Pennies");
+        }
+        [Test, Timeout(500)]
+        public void Test99()
+        {
+            Change amountAsChange = Program.ChangeAmount(.99m);
+            Assert.IsTrue(amountAsChange.Quarters == 3, "Incorrect Quarters");
+            Assert.IsTrue(amountAsChange.Dimes == 2, "Incorrect Dimes");
+            Assert.IsTrue(amountAsChange.Nickles == 0, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Pennies == 4, "Incorrect Pennies");
+        }
+
+        [Test, Timeout(500)]
+        public void Test1293()
+        {
+            Change amountAsChange = Program.ChangeAmount(12.93m);
+            Assert.IsTrue(amountAsChange.Quarters == 51, "Incorrect Quarters");
+            Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
+            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Pennies == 3, "Incorrect Pennies");
+        }
+
+        
+        
+    }
+}
